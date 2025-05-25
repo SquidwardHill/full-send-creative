0b24a9c9c618189e9033a3ccc11bf49b6b39190d
Sydney Hill <sydneyehill@gmail.com>
2025-05-25 08:32:08 -0700

    style refinements

--- app/components/titleTag.tsx
+++ app/components/titleTag.tsx
@@ -4,7 +4,7 @@ interface TitleTagProps {
   size?: string;
 }
 
-const TitleTag = ({ text, color = "text-cream-200", size = "text-2xl" }: TitleTagProps) => (
-  <p className={`font-sans ${size} tracking-[4px] ${color} pb-4`}>{text}</p>
+const TitleTag = ({ text, color = "text-cream-200", size = "text-[30px]" }: TitleTagProps) => (
+  <p className={`font-sans ${size} tracking-[3.5px] ${color} pb-4`}>{text}</p>
 );
 export default TitleTag;
