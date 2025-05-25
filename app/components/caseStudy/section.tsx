1d7ce36a5bb2e52959232e9189a50333fc43b289
Sydney Hill <sydneyehill@gmail.com>
2025-05-25 08:32:20 -0700

    style refinements

--- app/components/caseStudy/section.tsx
+++ app/components/caseStudy/section.tsx
@@ -6,7 +6,7 @@ interface ContentSectionProps {
 const ContentSection = ({ title, body = "🔨 WIP, coming soon!" }: ContentSectionProps) => (
   <section className="my-10">
     <h3 className="text-md font-light text-cream-200">{title}</h3>
-    <p className="mt-2 text-xl text-cream-100 text-normal">{body ?? "coming soon"}</p>
+    <p className="mt-2 text-lg text-cream-100 font-light">{body ?? "coming soon"}</p>
   </section>
 );
 export default ContentSection;
