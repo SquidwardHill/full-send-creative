d9ba3596892c701cc337db026bc8c853bfa007cb
Sydney Hill <sydneyehill@gmail.com>
2025-05-25 21:18:03 -0700

    Fetch asset images dynamically, move case study images to public folder

---
+++ app/utils/images.tsx
@@ -0,0 +1,8 @@
+import divider from "~/assets/images/hand-divider.png";
+import handsDivider from "~/assets/images/hands-stars-divider.png";
+import sparkle from "~/assets/images/sparkle.png";
+import spookyHand from "~/assets/images/spooky-hand.png";
+import aboutPicture from "~/assets/images/sh.png";
+import coverPlaceholder from "~/assets/images/cover-placeholder.png";
+
+export { divider, handsDivider, sparkle, spookyHand, aboutPicture, coverPlaceholder };
