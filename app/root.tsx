f59949494f9da901329a761d3b26d672d954ffd9
Sydney Hill <sydneyehill@gmail.com>
2025-08-10 06:28:52 -0700

    Font updates

--- app/root.tsx
+++ app/root.tsx
@@ -15,7 +15,11 @@ export default function App() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Red+Hat+Mono:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Rowdies:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Google+Sans+Mono:ital,wght@0,400;0,500;1,400;1,500&display=swap"
           rel="stylesheet"
         />
         <Meta />
